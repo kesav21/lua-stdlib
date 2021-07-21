27eeb750dee63416476a073c18c1dd6dd67166f9
Kesav Kadalazhi <kkadalazhi2@gmail.com>
2021-07-21 08:44:40 -0700

    feat: initial commit

---
+++ kk/fileio.lua
@@ -0,0 +1,35 @@
+local M = {}
+
+-- assumption: if the file can be opened, it exists
+function M.exists(filename)
+	local file = io.open(filename, 'r')
+	if file then
+		io.close(file)
+		return true
+	else
+		return false
+	end
+end
+
+function M.firstline(filename)
+	local lines = io.lines(filename)
+	local firstline = lines()
+	return firstline
+end
+
+function M.writeline(filename, data)
+	local file = io.open(filename, 'w')
+	if file then
+		io.output(file)
+		io.write(data)
+		return io.close(file)
+	else
+		return false
+	end
+end
+
+-- TODO: test all the functions in this module
+-- eq(true, fileio.writeline(statusbar_path, "kk"))
+-- eq("kk", fileio.firstline(statusbar_path))
+
+return M
